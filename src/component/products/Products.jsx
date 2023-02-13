81e3ad79af6f6fbed6b7f3860c6aa3897deb33bd
Mahmoud <forkan.saadaoui.gmail.com>
2023-02-13 21:33:44 +0100

    styling Products component

--- src/component/products/Products.jsx
+++ src/component/products/Products.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { storeData } from '../../assets/data/dummyData'
 import ProductCard from '../filteredProducts/ProductCard';
+import './products.css'
+import '../filteredProducts/filteredProducts.css'
 
 function Products() {
     // get the first product for each type
@@ -15,9 +17,10 @@ function Products() {
   return (
     <div className='products'>
       {
-        newStoreData.map((prod,index) => (
-            <ProductCard
-                key    = {index}
+        
+          newStoreData.map((prod,index) => (
+            <div className="single-product-card" key={index}>
+              <ProductCard
                 id     = {prod.id}
                 img    = {prod.img}
                 name   = {prod.name}
@@ -25,8 +28,10 @@ function Products() {
                 color  = {prod.color}
                 gender = {prod.gender}
                 price  = {prod.price}
-            />
+              />
+            </div>
           ))
+        
       }
     </div>
   )
